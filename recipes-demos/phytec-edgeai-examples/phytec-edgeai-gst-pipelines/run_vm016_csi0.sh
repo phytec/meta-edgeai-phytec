8e813392954ed690ea3fb2946839af17c6558abf
Steffen Hemer <s.hemer@phytec.de>
2025-03-26 09:54:57 +0100

    recipes-demos: phytec-edgeai-examples: Add remote sink also for the basic camera pipelines
    As for the edgeai pipelines, switching to a remote sink for straming the
    camera image, the variable needs to be replaced.
    
    Signed-off-by: Steffen Hemer <s.hemer@phytec.de>

--- recipes-demos/phytec-edgeai-examples/phytec-edgeai-gst-pipelines/run_vm016_csi0.sh
+++ recipes-demos/phytec-edgeai-examples/phytec-edgeai-gst-pipelines/run_vm016_csi0.sh
@@ -5,9 +5,12 @@ setup-pipeline-csi0
 WIDTH="1280"
 HEIGHT="800"
 
+LOCAL_SINK="kmssink driver-name=tidss sync=true connector-id=40"
+REMOTE_SINK="v4l2h264enc ! rtph264pay ! udpsink host=localhost port=8081 host=192.168.3.10"
+
 v4l2-ctl -d /dev/cam-csi0 -c autogain_analogue=1,auto_exposure=0
 
 gst-launch-1.0 v4l2src device=/dev/video-csi0 ! \
 video/x-bayer,format=grbg,width=${WIDTH},height=${HEIGHT} ! \
 bayer2rgb ! \
-kmssink driver-name=tidss sync=false connector-id=40
+${LOCAL_SINK}
