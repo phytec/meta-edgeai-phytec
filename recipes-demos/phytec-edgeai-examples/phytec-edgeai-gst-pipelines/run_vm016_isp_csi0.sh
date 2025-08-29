0c04c42f1af1677e0eea1dd65c9919bc9fd8fcc1
Steffen Hemer <s.hemer@phytec.de>
2025-08-29 09:39:50 +0200

    recipes-demos: phytec-edgeai-examples: Adopt to unified setup-pipeline-csi
    Need matching meta-phytec revision where setup-pipeline-csi for j721s2
    has been unified and take CSI as parameter. While at it, add CSI
    variable to all scripts.
    
    Signed-off-by: Steffen Hemer <s.hemer@phytec.de>

--- recipes-demos/phytec-edgeai-examples/phytec-edgeai-gst-pipelines/run_vm016_isp_csi0.sh
+++ recipes-demos/phytec-edgeai-examples/phytec-edgeai-gst-pipelines/run_vm016_isp_csi0.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
+CSI="0"
 BITS="12"
 MBUS_FMT="SGRBG${BITS}_1X${BITS}"
-setup-pipeline-csi0 -f ${MBUS_FMT}
+setup-pipeline-csi -i ${CSI} -f ${MBUS_FMT}
 
 BIN_PATH="/opt/imaging/ar0144_${BITS}bit/linear/"
 SENSOR_NAME="SENSOR_AR0144_PHYCAM"
@@ -14,11 +15,11 @@ HEIGHT="800"
 LOCAL_SINK="kmssink driver-name=tidss sync=true connector-id=40"
 REMOTE_SINK="v4l2h264enc ! rtph264pay ! udpsink host=localhost port=8081 host=192.168.3.10"
 
-v4l2-ctl -d /dev/cam-csi0 -c autogain_analogue=0,auto_exposure=1
+v4l2-ctl -d /dev/cam-csi${CSI} -c autogain_analogue=0,auto_exposure=1
 
-gst-launch-1.0 v4l2src device=/dev/video-csi0 io-mode=dmabuf-import ! \
+gst-launch-1.0 v4l2src device=/dev/video-csi${CSI} io-mode=dmabuf-import ! \
 video/x-bayer,format=${FMT},width=${WIDTH},height=${HEIGHT},framerate=607/10 ! \
-tiovxisp sink_0::device=/dev/cam-csi0 sensor-name=${SENSOR_NAME} \
+tiovxisp sink_0::device=/dev/cam-csi${CSI} sensor-name=${SENSOR_NAME} \
 dcc-isp-file=${BIN_PATH}dcc_viss.bin \
 sink_0::dcc-2a-file=${BIN_PATH}dcc_2a.bin format-msb=${FMT_MSB} ! \
 video/x-raw,format=NV12,width=${WIDTH},height=${HEIGHT},framerate=607/10 ! \
