1a601864ca89d219b7b428c96e269a905c08a9f0
Steffen Hemer <s.hemer@phytec.de>
2025-11-07 16:40:40 +0100

    recipes-demos: phytec-edgeai-gst-pipelines: receive_rtp_stream: Add port parameter
    To enable this script for multiple streams, it now takes a UDP port
    parameter (>=1024) instead of the fixed default.
    
    Signed-off-by: Steffen Hemer <s.hemer@phytec.de>

--- recipes-demos/phytec-edgeai-examples/phytec-edgeai-gst-pipelines/receive_rtp_stream.sh
+++ recipes-demos/phytec-edgeai-examples/phytec-edgeai-gst-pipelines/receive_rtp_stream.sh
@@ -5,5 +5,14 @@ if [ "${HW_ARCH}" = "aarch64" ]; then
 	echo "You are likely running this on the target. This script is meant to be run on your host. Copy it there."
 	exit
 fi
-gst-launch-1.0 udpsrc port=8081 ! application/x-rtp,media=video,clock-rate=90000,encoding-name=H264,payload=96 ! \
+
+UDP_PORT=8081
+if [ $1 -ge 1024 ]; then
+	UDP_PORT=$1
+	echo "Listen for stream on port ${UDP_PORT}"
+else
+	echo "Listen on default port ${UDP_PORT}"
+fi
+
+gst-launch-1.0 udpsrc port=${UDP_PORT} ! application/x-rtp,media=video,clock-rate=90000,encoding-name=H264,payload=96 ! \
 rtph264depay ! h264parse ! queue leaky=2 ! avdec_h264 ! queue leaky=2 ! videoconvert ! autovideosink sync=false
