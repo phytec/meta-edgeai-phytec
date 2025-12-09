2f77e109a708c710153b6fb4a803f84a53f01629
Steffen Hemer <s.hemer@phytec.de>
2025-12-09 17:33:36 +0100

    recipes-demos: phytec-edgeai-examples: Add two phyCAM-L AI examples
    gst examples needs unversioned .so symlinks (from some dev package),
    better correctly link the respective lib/app.
    
    Signed-off-by: Steffen Hemer <s.hemer@phytec.de>

---
+++ recipes-demos/phytec-edgeai-examples/phytec-edgeai-gst-pipelines/run_vm016_isp_csi0_port0_object_det.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+CSI="0"
+PORT="0"
+BITS="12"
+MBUS_FMT="SGRBG${BITS}_1X${BITS}"
+setup-pipeline-csi -i ${CSI} -f ${MBUS_FMT} -p ${PORT}
+if [ "$?" = '1' ]; then
+	echo "Error on pipeline setup!"; exit 1
+fi
+
+BIN_PATH="/opt/imaging/ar0144_${BITS}bit/linear/"
+SENSOR_NAME="SENSOR_AR0144_PHYCAM"
+FMT="grbg${BITS}"
+FMT_MSB="$((${BITS}-1))"
+WIDTH="1280"
+HEIGHT="800"
+
+LOCAL_SINK="kmssink driver-name=tidss sync=true connector-id=40"
+REMOTE_SINK="v4l2h264enc ! rtph264pay ! udpsink host=localhost port=8081 host=192.168.3.10"
+
+v4l2-ctl -d /dev/cam-csi${CSI}-port${PORT} -c autogain_analogue=0,auto_exposure=1
+
+gst-launch-1.0 v4l2src device=/dev/video-csi${CSI}-port${PORT} io-mode=dmabuf-import ! \
+video/x-bayer,format=${FMT},width=${WIDTH},height=${HEIGHT},framerate=607/10 ! \
+tiovxisp sink_0::device=/dev/cam-csi${CSI}-port${PORT} sensor-name=${SENSOR_NAME} \
+dcc-isp-file=${BIN_PATH}dcc_viss.bin \
+sink_0::dcc-2a-file=${BIN_PATH}dcc_2a.bin format-msb=${FMT_MSB} ! \
+video/x-raw,format=NV12,width=${WIDTH},height=${HEIGHT},framerate=607/10 ! \
+tiovxmemalloc pool-size=12 ! \
+tiovxmultiscaler name=split_01 src_0::roi-startx=0 src_0::roi-starty=0 src_0::roi-width=${WIDTH} src_0::roi-height=${HEIGHT} target=0 \
+split_01. ! queue ! video/x-raw, width=416, height=416 ! tiovxdlpreproc model=/opt/model_zoo/ONR-OD-8200-yolox-nano-lite-mmdet-coco-416x416  out-pool-size=4 ! \
+application/x-tensor-tiovx ! tidlinferer target=1  model=/opt/model_zoo/ONR-OD-8200-yolox-nano-lite-mmdet-coco-416x416 ! post_0.tensor \
+split_01. ! queue ! video/x-raw, width=${WIDTH}, height=${HEIGHT} ! post_0.sink \
+tidlpostproc name=post_0 model=/opt/model_zoo/ONR-OD-8200-yolox-nano-lite-mmdet-coco-416x416 alpha=0.200000 viz-threshold=0.600000 top-N=5 ! queue ! mosaic_0. \
+tiovxmosaic name=mosaic_0 target=1 src::pool-size=4 \
+sink_0::startx="<0>" sink_0::starty="<0>" sink_0::widths="<${WIDTH}>" sink_0::heights="<${HEIGHT}>" \
+! video/x-raw,format=NV12, width=${WIDTH}, height=${HEIGHT} ! queue ! tiperfoverlay title="Phycam VM-016 Object Detection" ! \
+${REMOTE_SINK}
