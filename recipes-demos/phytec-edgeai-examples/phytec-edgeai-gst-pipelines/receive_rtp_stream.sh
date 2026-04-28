5b295dc483dbb7c2311f5d0d0f6cd8b521500093
Steffen Hemer <s.hemer@phytec.de>
2026-04-28 14:58:24 +0200

    HACK: recipes-demos: phytec-edgeai-gst-pipelines: Periodically save images for LDC calibratiton
    Signed-off-by: Steffen Hemer <s.hemer@phytec.de>

--- recipes-demos/phytec-edgeai-examples/phytec-edgeai-gst-pipelines/receive_rtp_stream.sh
+++ recipes-demos/phytec-edgeai-examples/phytec-edgeai-gst-pipelines/receive_rtp_stream.sh
@@ -14,5 +14,14 @@ else
 	echo "Listen on default port ${UDP_PORT}"
 fi
 
+#export GST_DEBUG_DUMP_DOT_DIR=/tmp/gst-dot
+SAVE_IMAGES=0 #set to 1
+if [ ${SAVE_IMAGES} -gt 0 ]; then
+	mkdir -p /tmp/ldc/
+	PIPELINE_END="tee name=t ! queue ! autovideosink sync=false t. ! queue name=png ! videoconvert ! video/x-raw,format=RGB ! videorate ! video/x-raw,framerate=1/3 ! pngenc ! multifilesink location=/tmp/ldc/file-%02d.png"
+else
+	PIPELINE_END="videoconvert ! autovideosink sync=false"
+fi
+
 gst-launch-1.0 udpsrc port=${UDP_PORT} ! application/x-rtp,media=video,clock-rate=90000,encoding-name=H264,payload=96 ! \
-rtph264depay ! h264parse ! queue leaky=2 ! avdec_h264 ! queue leaky=2 ! videoconvert ! autovideosink sync=false
+rtph264depay ! h264parse ! queue leaky=2 ! avdec_h264 ! queue leaky=2 ! ${PIPELINE_END}
